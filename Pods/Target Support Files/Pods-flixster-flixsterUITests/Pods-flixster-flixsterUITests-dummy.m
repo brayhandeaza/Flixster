affc05d9bb98f61e248f9e3a03207701347b675d
Brayhan De Aza <brayhandeaza@gmail.com>
2020-09-22 02:32:49 -0400

    img size fixed

---
+++ Pods/Target Support Files/Pods-flixster-flixsterUITests/Pods-flixster-flixsterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_flixster_flixsterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_flixster_flixsterUITests
+@end
---
+++ Pods/Target Support Files/Pods-flixsterTests/Pods-flixsterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_flixsterTests : NSObject
+@end
+@implementation PodsDummy_Pods_flixsterTests
+@end

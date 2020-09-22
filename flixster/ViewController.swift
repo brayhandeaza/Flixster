6708e0427a48eed10006698d9295c223410afcc9
Brayhan De Aza <brayhandeaza@gmail.com>
2020-09-22 01:12:02 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Brayhan De Aza on 9/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
